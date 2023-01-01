0e59bc3e3453e1dd48d1d22384aa900ad3ed794d
requiemcreatif <requiemcreatif@gmail.com>
2023-01-01 20:44:29 +0100

    meta tags updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import { useState } from "react";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
@@ -22,43 +23,57 @@ function App() {
   const isDarkTheme = theme === "dark";
   const toggleTheme = () => setTheme(isDarkTheme ? "light" : "dark");
   return (
-    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
-      <div>
-        <GlobalStyles />
-        <BrowserRouter>
-          {/* <Navigation
+    <HelmetProvider>
+      <Helmet>
+        <title>Requiem Creatif - Full stack development by Alain Mani</title>
+        <meta
+          name="description"
+          content="Requiemcreatif is about web development, programming, and technology."
+        />
+        <meta
+          name="keywords"
+          content="Full stack development by build web and apps with JavaScript, React, Node, Express, MongoDB, and more."
+        />
+        <meta name="author" content="Alain Mani" />
+      </Helmet>
+      <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+        <div>
+          <GlobalStyles />
+          <BrowserRouter>
+            {/* <Navigation
             toggleTheme={toggleTheme}
             isDarkTheme={isDarkTheme}
             toggleShowLinks={toggleShowLinks}
             showLinks={showLinks}
           /> */}
-          {/* <NavBarFirst toggleTheme={toggleTheme} isDarkTheme={isDarkTheme} /> */}
-          {/* <Navbar toggleTheme={toggleTheme} isDarkTheme={isDarkTheme} /> */}
-          {/* <Header />
+            {/* <NavBarFirst toggleTheme={toggleTheme} isDarkTheme={isDarkTheme} /> */}
+            {/* <Navbar toggleTheme={toggleTheme} isDarkTheme={isDarkTheme} /> */}
+            {/* <Header />
           <Cards />
           <Contactform /> */}
-          {/* <Home /> */}
-          {/* <Contact /> */}
+            {/* <Home /> */}
+            {/* <Contact /> */}
 
-          {/* <Footer /> */}
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <Home
-                  toggleTheme={toggleTheme}
-                  isDarkTheme={isDarkTheme}
-                  toggleShowLinks={toggleShowLinks}
-                  showLinks={showLinks}
-                />
-              }
-            />
+            {/* <Footer /> */}
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <Home
+                    toggleTheme={toggleTheme}
+                    isDarkTheme={isDarkTheme}
+                    toggleShowLinks={toggleShowLinks}
+                    showLinks={showLinks}
+                  />
+                }
+              />
 
-            <Route path="/blog" element={<Blog />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </ThemeProvider>
+              <Route path="/blog" element={<Blog />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </ThemeProvider>
+    </HelmetProvider>
   );
 }
 
